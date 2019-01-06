451333a7177e4c7ed798132c4a64610f30bcee85
kspayne93 <kyle@investavante.com>
2019-01-06 10:05:57 -0700

    added dummy data insertion

--- db/seed.sql
+++ db/seed.sql
@@ -3,7 +3,20 @@ create table punches(
     id serial PRIMARY KEY,
     punch_status varchar(10),
     punch_date date,
-    day varchar(10),
+    punch_day varchar(10),
     punch_time time
 );
 
+
+-- Adding dummy data
+insert into punches(punch_status, punch_date, punch_day, punch_time)
+values('in', '2018-01-04', 'Friday', '08:00:00');
+
+insert into punches(punch_status, punch_date, punch_day, punch_time)
+values('out', '2018-01-04', 'Friday', '05:00:00');
+
+insert into punches(punch_status, punch_date, punch_day, punch_time)
+values('in', '2018-01-0', 'Saturday', '11:30:00');
+
+insert into punches(punch_status, punch_date, punch_day, punch_time)
+values('out', '2018-01-0', 'Saturday', '04:30:00');
