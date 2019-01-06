6c77b9abbe69c575cb141450ac830f4bb7e510c1
kspayne93 <kyle@investavante.com>
2019-01-06 10:07:05 -0700

    added getAll (get), createItem (post), and deleteItem(delete) methods

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,29 @@
 module.exports = {
+   getAll: (req, res) => {
+      const db = req.app.get('db')
+
+      db.get_all_punches().then(response => {
+         res.status(200).send(response)
+      })
+   },
+
+   createItem: (req, res) => {
+      const db = req.app.get('db')
+      let {punch_status, punch_date, punch_day, punch_time} = req.body //deconstructing incoming request properties off of body 
+
+      db.create_punch({punch_status, punch_date, punch_day, punch_time}).then(response => {
+         res.status(200).send(response) //adding new request properties to database, then sending response with updated database
+      })
+   },
+
+   deleteItem: (req, res) => {
+      const db = req.app.get('db')
+      let {id} = req.params // id is being deconstructed off of incoming params
+
+      db.delete_punch({id}).then(response => {
+         res.status(200).send(response)
+      })
+   },
+
    
 }
