d4c0a1784068a36ce3477fd87045271656ad27c2
kspayne93 <kyle@investavante.com>
2019-01-08 10:10:06 -0700

    added new expected values

--- db/create_punch.sql
+++ db/create_punch.sql
@@ -1,4 +1,4 @@
-INSERT INTO punches(punch_status, punch_date, punch_day, punch_time)
-VALUES ($(punch_status), $(punch_date), $(punch_day), $(punch_time));
+INSERT INTO punches(punch_status, punch_date, punch_day, punch_time, am_pm)
+VALUES ($(punch_status), $(punch_date), $(punch_day), $(punch_time), $(am_pm));
 
 select * from punches;
