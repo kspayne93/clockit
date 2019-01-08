8d83fc2b247b410a970e4a3e0bc359425abd0c46
kspayne93 <kyle@investavante.com>
2019-01-08 10:09:13 -0700

    updated data types to be strings

--- db/seed.sql
+++ db/seed.sql
@@ -2,21 +2,22 @@
 create table punches(
     id serial PRIMARY KEY,
     punch_status varchar(10),
-    punch_date date,
+    punch_date varchar(8),
     punch_day varchar(10),
-    punch_time time
+    punch_time varchar(8),
+    am_pm varchar(2)
 );
 
 
 -- Adding dummy data
-insert into punches(punch_status, punch_date, punch_day, punch_time)
-values('in', '2018-01-04', 'Friday', '08:00:00');
+insert into punches(punch_status, punch_date, punch_day, punch_time, am_pm)
+values('in', '01/04/18', 'Friday', '08:00','am');
 
-insert into punches(punch_status, punch_date, punch_day, punch_time)
-values('out', '2018-01-04', 'Friday', '05:00:00');
+insert into punches(punch_status, punch_date, punch_day, punch_time, am_pm)
+values('out', '1/04/18', 'Friday', '05:00','pm');
 
-insert into punches(punch_status, punch_date, punch_day, punch_time)
-values('in', '2018-01-0', 'Saturday', '11:30:00');
+insert into punches(punch_status, punch_date, punch_day, punch_time, am_pm)
+values('in', '1/05/18', 'Saturday', '11:30', 'am');
 
-insert into punches(punch_status, punch_date, punch_day, punch_time)
-values('out', '2018-01-0', 'Saturday', '04:30:00');
+insert into punches(punch_status, punch_date, punch_day, punch_time, am_pm)
+values('out', '1/05/18', 'Saturday', '04:30', 'pm');
